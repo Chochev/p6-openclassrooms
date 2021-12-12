990ed1097e3f8f5ce97ec2358ba7d26a74c02685
Ivan Tchotchev <ivan_dirst@yahoo.com>
2021-12-12 16:06:08 +0100

    upgrade the like/dislike fnc

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -104,7 +104,7 @@ exports.likeSauce = (req, res, next) => {
                 _id: sauceId
             }, {
                 $push: {
-                    userDisliked: userId
+                    usersDisliked: userId
                 },
                 $inc: {
                     dislikes: +1
@@ -120,50 +120,50 @@ exports.likeSauce = (req, res, next) => {
             error
           }))
     }
-    // if (like === 0) {
-    //     Sauce.findOne({
-    //         _id: sauceId
-    //     })
-    //     .then((sauce) => {
-    //         if(sauce.usersLiked.includes(userId)) {
-    //             Sauce.updateOne({
-    //                 _id: sauceId
-    //             }, {
-    //                 $pull: {
-    //                     usersLiked: userId
-    //                 }, 
-    //                  $inc: {
-    //                     likes: -1
-    //                  },
-    //             })
-    //             .then(() => res.status(200).json({
-    //                 message: 'Like delete !'
-    //               }))
-    //               .catch((error) => res.status(400).json({
-    //                 error
-    //               }))
-    //         }
-    //         if(sauce.userDisliked.includes(userId)) {
-    //             Sauce.updateOne({
-    //                 _id: sauceId
-    //             }, {
-    //                 $pull: {
-    //                     userDisliked: userId
-    //                 },
-    //                 $inc: {
-    //                     dislikes: -1
-    //                 },
-    //             })
-    //             .then(() => res.status(200).json({
-    //                 message: 'dislike delete !'
-    //               }))
-    //               .catch((error) => res.status(400).json({
-    //                 error
-    //               }))
-    //         }
-    //     })
-    //     .catch((error) => res.status(404).json({
-    //         error
-    //       }))
-    // }
+    if (like === 0) {
+        Sauce.findOne({
+            _id: sauceId
+        })
+        .then((sauce) => {
+            if(sauce.usersLiked.includes(userId)) {
+                Sauce.updateOne({
+                    _id: sauceId
+                }, {
+                    $pull: {
+                        usersLiked: userId
+                    }, 
+                     $inc: {
+                        likes: -1
+                     },
+                })
+                .then(() => res.status(200).json({
+                    message: 'Like delete !'
+                  }))
+                  .catch((error) => res.status(400).json({
+                    error
+                  }))
+            }
+            if(sauce.usersDisliked.includes(userId)) {
+                Sauce.updateOne({
+                    _id: sauceId
+                }, {
+                    $pull: {
+                        usersDisliked: userId
+                    },
+                    $inc: {
+                        dislikes: -1
+                    },
+                })
+                .then(() => res.status(200).json({
+                    message: 'dislike delete !'
+                  }))
+                  .catch((error) => res.status(400).json({
+                    error
+                  }))
+            }
+        })
+        .catch((error) => res.status(404).json({
+            error
+          }))
+    }
 }
